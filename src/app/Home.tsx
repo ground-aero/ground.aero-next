9923eac760896719d5fdc424f9679bd68b026be9
unknown <ea.avdeev@gmail.com>
2024-07-01 13:08:11 +0300

    fix(home): clean code

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -5,6 +5,7 @@ import "../app/globals.css"
 import styles from "./page.module.css"
 import {Header, Nav, Main, LayoutMainHome, Footer} from "@/components"
 import {intro} from "@/constants"
+import Link from 'next/link'
 
   type PageProps = {
     events: {id: number, title: string, body: string}[],
@@ -30,91 +31,8 @@ const HomePage:FC<PageProps> = ({events, children}) => {return (
           titleEvents={intro.main.titleEvents}
           events={events}>
         </LayoutMainHome>
-      </Main>
 
-      <Footer title='. F'></Footer>
-    </>
-
-   )
-}
-
-export default HomePage
-
-      {/* <main className={styles.main}>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.description}>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/images/groundaero_logoline12.png"
-                alt="ground Logo"
-                className={styles.groundLogo}
-                width={300}
-                height={40}
-                priority
-              />
-            </a>
-          </div>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/app/page.tsx</code>
-          </p>
-          
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>Explore starter templates for Next.js.</p>
-          </a>
-
-          <Link
+        <Link
             href="/library"
             className={styles.card}
             target="_blank"
@@ -127,6 +45,13 @@ export default HomePage
               Link to my ground,.aero Library
             </p>
           </Link>
-        </div>
+          
+      </Main>
 
-      </main> */}
+      <Footer title='. F'></Footer>
+    </>
+
+   )
+}
+
+export default HomePage
