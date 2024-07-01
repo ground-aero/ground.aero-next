74ce3f5b912f7b149f0f3d9a7102e75ee51c268d
unknown <ea.avdeev@gmail.com>
2024-07-01 13:59:51 +0300

    refactor(layoutmainhome): add links effect

--- src/components/LayoutMainHome.tsx
+++ src/components/LayoutMainHome.tsx
@@ -31,14 +31,14 @@ export const LayoutMainHome: React.FC<LayoutMainProps> = ({ layout, title, text,
 {/* INTRO Section ----------------------------------------------*/}
 {/* left */}
           <div id={styles.introBoxLeft} className={styles.boxAside}>
-            <div className={styles.emptyBoxLeft}>
+            <Link href="#" className={`${styles.card} ${styles.emptyBoxRight}`} target="_self" rel="noopener noreferrer">
               <div className={styles.innerEmpty}>
                   <Image
                     src="/images/facts_n_figures.png" alt="facts & figures" width={28} height={28}
                   />
                   <h3 className={styles.intro_title}>{titleFacts}</h3>
               </div>
-            </div>
+            </Link>
             
             <ul className={styles.intro_list}>
               <li className={`${styles.intro_text} ${styles.intro_text_left}`}>{textFacts.factOne}</li>
@@ -60,14 +60,14 @@ export const LayoutMainHome: React.FC<LayoutMainProps> = ({ layout, title, text,
 {/* right */}
           <aside id={styles.introBoxRight} className={styles.boxAside}>
 
-            <div className={styles.emptyBoxRight}>
+            <Link href="/library" className={`${styles.card} ${styles.emptyBoxRight}`} target="_self" rel="noopener noreferrer">
               <div className={styles.innerEmpty}> 
                 <h3 className={styles.intro_title}>{titlePublications}</h3>
                 <Image
                     src="/images/arrows2.png" className={styles.imgFacts} alt="arrows" width={28} height={28}
                   />                      
               </div>
-            </div>
+            </Link>
 
             <div className={styles.intro_list}>
               <h2 className={`${styles.intro_title} ${styles.intro_title_right}`}>SGHA2018</h2>
