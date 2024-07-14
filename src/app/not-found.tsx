892bfa83689405bff56a2a153985f90f4ba7a0c5
unknown <ea.avdeev@gmail.com>
2024-07-14 23:20:21 +0300

    fix(src):not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,17 @@
+import { Main, Footer, LayoutMainLibrary } from "@/components"
 import styles from "./page.module.css"
+import {intro} from '@/constants'
 
 export default function NotFound() {
     return (
-    <div className={styles.centerColumn}>
-      <h2 className="text-xl font-bold">Page not found</h2>
-      <p className="text-xl font-bold">Could not find requested resource</p>
-    </div>
+      <>
+          <div className={styles.center}>
+            <div className={styles.centerColumn}>
+              <h2>Page not found</h2>
+              <p>Could not find requested resource</p>
+            </div>
+          </div>  
+          <Footer/>
+      </>
   );
-
 }
