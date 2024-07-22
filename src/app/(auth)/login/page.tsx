ed8b4dfd44435f6f247c0450612a5a1741774bf5
unknown <ea.avdeev@gmail.com>
2024-07-22 23:21:24 +0300

    feat(app/auth) implement auth routes

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../../page.module.css'
+
+export default function Login() {
+    return (
+    <div className={styles.centerColumn}>
+        <h1>Login</h1>
+        <p>Enter your email and password to login.</p>
+    </div>
+  );
+
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+import styles from '../../page.module.css'
+
+export default function Register() {
+    return (
+        <div className={styles.centerColumn}>
+            <h1>Register</h1>
+            <p>Enter your email and password to register.</p>
+        </div>
+      );
+}
