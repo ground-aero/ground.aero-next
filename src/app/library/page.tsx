6903d89a7226fbae044c225876d1c70e60967026
unknown <ea.avdeev@gmail.com>
2024-07-23 11:17:33 +0300

    feat(library): add metadata

--- src/app/library/page.tsx
+++ src/app/library/page.tsx
@@ -1,6 +1,11 @@
 // LIBRARY - HOME PAGE. отвеч.за серверную часть
-import { NextPage } from 'next'
+import { NextPage, Metadata } from 'next'
 import Library from './Library'
+
+export const metadata: Metadata = {
+  title: "IATA sgha 2018 pdf dowload, sgha 2013 IATA pdf, Library",
+  description: "Directory. 1. SGHA 2018 IATA, 2. SGHA 2013 IATA, 3. Ground Handling Vocab",
+};
 
 const LibraryPage: NextPage = () => {
     return (
