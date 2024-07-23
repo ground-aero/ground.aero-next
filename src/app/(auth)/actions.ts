4cd3df3dff4ce01f570037de4e0880b3c7faadc5
unknown <ea.avdeev@gmail.com>
2024-07-23 15:07:28 +0300

    feat(register,login): add actions, mocked

---
+++ src/app/(auth)/actions.ts
@@ -0,0 +1,9 @@
+'use server';
+
+export async function register() {
+    return console.log('You\'ve sent it to register!')
+}
+
+export async function login() {
+    return console.log('You\'ve logged in!')
+}
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { login } from '../actions'
+import Link from 'next/link'
 import styles from '../../page.module.css'
 
 export default function Login() {
@@ -5,6 +8,21 @@ export default function Login() {
     <div className={styles.centerColumn}>
         <h1>Login</h1>
         <p>Enter your email and password to login.</p>
+
+            {/* <form onSubmit={send}> */}
+            <form action={ login }>
+                <button type="submit">Login</button>
+            </form>
+            
+            <br/>
+            <p>Do not have an account?       
+                <Link href="/register" className={styles.nav} rel="noopener noreferrer">
+                    Register
+                </Link>
+            </p>
+
+            <p></p> 
+
     </div>
   );
 
