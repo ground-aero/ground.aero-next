f730b3a7fb1ce5c6e631e34660df69a99eacb92b
unknown <ea.avdeev@gmail.com>
2024-09-02 21:34:33 +0300

    build(public): add robots.txt, sitemap.xml

--- public/not-found.tsx
+++ public/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
     <div className={styles.centerColumn}>
       <h2 className="text-xl font-bold">Page not found</h2>
-      <p className="text-xl font-bold">Could not find requested resource</p>
+      <p className="text-xl font-bold">Could not find requested resource old.</p>
     </div>
   );
 
