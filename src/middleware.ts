7c73799deb2db5b91f30f3ca06b7ce49fd7e4b95
unknown <ea.avdeev@gmail.com>
2024-09-03 09:37:07 +0300

    refactor(routes): redirect publications_sgha_2018_overview to sgha2018

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,17 +3,23 @@ import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()
-  console.log('Middleware invoked for:', url.pathname)
+    console.log('Middleware invoked for:', url.pathname)
 
-  if (url.pathname === '/services' || url.pathname === '/agenda' || url.pathname === '/publications_sgha_2018_overview' || url.pathname === '/events2023' || url.pathname === '/events2022' || url.pathname === '/events2021') {
-    console.log('Redirecting to home page from:', url.pathname)
+  if (url.pathname === '/services' || url.pathname === '/agenda' || url.pathname === '/events2023' || url.pathname === '/events2022' || url.pathname === '/events2021') {
+      console.log('Redirecting to home page from:', url.pathname)
     const homeUrl = new URL('/', url)
     return NextResponse.redirect(homeUrl)
   }
+
+  if (url.pathname === '/publications_sgha_2018_overview') {
+    console.log('Redirecting to sgha2018 page from:', url.pathname)
+  const sgha2018Url = new URL('/sgha2018', url)
+  return NextResponse.redirect(sgha2018Url)
+}
 
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/services', '/agenda', '/publications_sgha_2018_overview'],
+  matcher: ['/services', '/agenda', '/publications_sgha_2018_overview', '/events2023', '/events2022', '/events2021'],
 }
